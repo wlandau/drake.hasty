28cfd5adf685cd36c753496df1fc5f9fc2a7bccb
wlandau <will.landau@gmail.com>
2019-01-03 20:22:05 -0500

    Pass some checks

--- tests/testthat/test-hasty.R
+++ tests/testthat/test-hasty.R
@@ -1,50 +1,55 @@
 context("hasty")
 
-drake::test_with_dir("hasty parallelism", {
-  skip_on_cran()
-  load_mtcars_example()
-  my_plan$command[my_plan$target == "report"] <-
-    "utils::write.csv(coef_regression2_large, file = file_out(\"coef.csv\"))"
-  options(clustermq.scheduler = "multicore")
-  for (jobs in 1:2) {
-    if (jobs > 1) {
-      skip_on_os("windows")
-      skip_if_not_installed("clustermq")
-      if ("package:clustermq" %in% search()) {
-        detach("package:clustermq", unload = TRUE) # nolint
+test_that("hasty parallelism", {
+  dir <- tempfile()
+  dir.create(dir)
+  withr::with_dir(dir, {
+    load_mtcars_example()
+    my_plan$command[my_plan$target == "report"] <-
+      "utils::write.csv(coef_regression2_large, file = file_out(\"coef.csv\"))"
+    options(clustermq.scheduler = "multicore")
+    config <- drake_config(my_plan, parallelism = backend_hasty)
+    for (jobs in 1:2) {
+      config$hasty_build <- default_hasty_build
+      if (jobs > 1) {
+        skip_on_os("windows")
+        skip_if_not_installed("clustermq")
+        if ("package:clustermq" %in% search()) {
+          detach("package:clustermq", unload = TRUE) # nolint
+        }
       }
+      config$jobs <- jobs
+      # default build function
+      expect_false(file.exists("coef.csv"))
+      expect_warning(make(config = config), regexp = "USE AT YOUR OWN RISK")
+      expect_true(file.exists("coef.csv"))
+      expect_equal(length(intersect(my_plan$target, cached())), 0)
+      unlink("coef.csv")
+      expect_false(file.exists("coef.csv"))
+      # remote_hasty_build()
+      o <- remote_hasty_build(
+        target = "small",
+        deps = list(simulate = simulate),
+        config = config
+      )
+      expect_true(is.data.frame(o$value))
+      # custom build function
+      expect_false(file.exists("small"))
+      config$hasty_build <- function(target, config) {
+        file.create(target)
+      }
+      expect_warning(
+        make(config = config),
+        regexp = "USE AT YOUR OWN RISK"
+      )
+      expect_true(file.exists("small"))
+      unlink("small")
+      expect_false(file.exists("small"))
+      expect_equal(length(intersect(my_plan$target, cached())), 0)
+      expect_false(file.exists("coef.csv"))
     }
-    # default build function
-    expect_false(file.exists("coef.csv"))
-    expect_warning(
-      make(my_plan, envir = e, parallelism = "hasty", jobs = jobs),
-      regexp = "USE AT YOUR OWN RISK"
-    )
-    expect_true(file.exists("coef.csv"))
-    expect_equal(length(intersect(my_plan$target, cached())), 0)
-    unlink("coef.csv")
-    expect_false(file.exists("coef.csv"))
-    # custom build function
-    expect_false(file.exists("small"))
-    hasty_write <- function(target, config) {
-      file.create(target)
+    if ("package:clustermq" %in% search()) {
+      detach("package:clustermq", unload = TRUE) # nolint
     }
-    expect_warning(
-      make(
-        my_plan,
-        envir = e,
-        parallelism = "hasty",
-        hasty_build = hasty_write
-      ),
-      regexp = "USE AT YOUR OWN RISK"
-    )
-    expect_true(file.exists("small"))
-    unlink("small")
-    expect_false(file.exists("small"))
-    expect_equal(length(intersect(my_plan$target, cached())), 0)
-    expect_false(file.exists("coef.csv"))
-  }
-  if ("package:clustermq" %in% search()) {
-    detach("package:clustermq", unload = TRUE) # nolint
-  }
+  })
 })
